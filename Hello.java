be05ae90aa90c21ff4fed20fa97f4e162d487db9
ycj <494136313@qq.com>
2018-05-20 11:22:51 +0800

    增加两行，减少一行

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
 	public static void main (String[] args)
 	{
-		System.out.print("hello world");
+		System.out.print("hello worldDSAFASDFASDF");
+		System.out.print("hello worldSDF");
 	}
 }
