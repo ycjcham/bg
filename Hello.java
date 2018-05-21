064b15d4fdbb8eac009bf9b34f483492fe9b6a95
ycj <494136313@qq.com>
2018-05-21 22:20:37 +0800

    master update

--- Hello.java
+++ Hello.java
@@ -2,8 +2,8 @@ public class Hello
 {
 	public static void main (String[] args)
 	{
+		System.out.print("hello world1222222222222");
 		System.out.print("hello world1111111111111");
-		System.out.print("hello world1111111111111");
-		System.out.print("hello world1111111111111");
+		System.out.print("hello world33333333333333");
 	}
 }
